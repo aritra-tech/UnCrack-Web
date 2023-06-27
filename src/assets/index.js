46e290209d7299b74dccac9fd93728f9d3a0f9d9
Aritra Das <aritrarick2002@gmail.com>
2023-06-27 21:19:15 +0530

    Created the About section

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg"
+import home from "./home.svg"
+import discount from "./Discount.svg"
+import google from "./google.svg"
+
+export {
+    logo,
+    home,
+    discount,
+    google
+}
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import styles, { layout } from '../style'
+import {home,google} from '../assets'
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about' className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+
+        <img src={home} alt="home" className='w-[90%] h-[90%] relative z-[5]'/>
+
+        {/* gradient start */}
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+        {/* gradient end */}
+
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h1 className={styles.heading2}>
+            So, what is{" "}
+            <span className='text-gradient'>UnCrack ?</span> {" "}
+        </h1>
+        <p className={`${styles.paragraph} mx-w-[490px] mt-5`}>
+        Uncrck unlock the power of secure informtion with ease! 
+        This innovative app securely stores all your login credentials and other important 
+        information, so you never have to worry about forgetting passwords or searching for lost data. 
+        Keep your digital life organized and protected with UnCrack.
+        </p>
+
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img src={google} alt="google_play" />
+      </div>
+
+      </div>
+    </section>
   )
 }
 
