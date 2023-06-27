43380ec1f7d229f52b704d070aaa62ae43feff64
Aritra Das <aritrarick2002@gmail.com>
2023-06-27 21:17:47 +0530

    Added some gradient

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
 import styles from '../style'
 import { motion } from "framer-motion"
-import discount from '../assets/Discount.svg'
-import home from '../assets/home.svg'
+import {discount,home} from '../assets'
 import Button from './Button'
 
 const Home = () => {
@@ -40,6 +39,12 @@ const Home = () => {
           translate='yes'
         >
           <img src={home} alt="uncrack" className="w-[90%] h-[90%] relative z-[5]" />
+
+          {/* gradient start */}
+          <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+          <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+          {/* gradient end */}
+
       </motion.div>
       </div>
     </section>
