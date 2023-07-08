c0833fc0824cfce13e372bd85deca9113d9fd5a4
Aritra Das <aritrarick2002@gmail.com>
2023-07-09 02:10:18 +0530

    Made some chnages in NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,38 @@
-import {useState} from 'react';
-// import { logo } from '../assets';
-import logo from '../assets/logo.svg'
+import { useState } from 'react';
+import logo from '../assets/logo.svg';
 import { navLinks } from '../const';
 
-
 const NavBar = () => {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  // Set the active index to 0 initially
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <nav className="w-full flex py-7 justify-between items-center navbar">
-      <img src={ logo } alt="Uncrack" className='w-[100px] h-[50px]'/>
+      <img src={logo} alt="Uncrack" className="w-[100px] h-[50px]" />
 
-      <ul className='list-none sm:flex hidden 
-      justify-end items-center flex-1'>
-        {navLinks.map((nav,index) => (
-          <li 
-          key={nav.id} 
-          className={`font-poppins 
-          font-normal cursor-pointer 
-          text-[17px] ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}
-          text-white`}
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[17px] ${
+              index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
+            } ${activeIndex === index ? 'text-gradient' : 'text-white'}`}
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
           >
-            <a href={`#${nav.id}`}>
+            <a
+              href={`#${nav.id}`}
+              onClick={() => setActiveIndex(index)} // Set the active index on click
+            >
               {nav.title}
             </a>
           </li>
         ))}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
